cd27add13c7a79aa9456119633c6962012ca4586
theotsori <theodoretsori@gmail.com>
2023-01-20 15:04:22 +0300

    Sorting header

--- sort.h
+++ sort.h
@@ -18,4 +18,11 @@ typedef struct listint_s
 #include <stdlib.h>
 #include <stdio.h>
 
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
 #endif /*sort.h*/
