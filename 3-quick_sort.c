ee273c6970baf468f34c5fbe843713281015e712
theotsori <theodoretsori@gmail.com>
2023-01-25 13:43:46 +0300

    quick sort nt updatex1

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,50 +1,40 @@
 #include "sort.h"
 
 /**
- * quick_sort_h - function as quick sort in lomunto
- * @array: numbers to be sorted
- * @low: first lowest number to compare
- * @high: largest num to compare
- * @size: memory used by the algorithm
- */
-void quick_sort_h(int *array, int low, int high, size_t size)
-{
-	if (low < high)
-	{
-		int pivot = array[high];
-		int i = low - 1;
-		int temp;
-		int j;
-
-		for (j = low; j <= high - 1; j++)
-		{
-			if (array[j] <= pivot)
-			{
-				i++;
-				if (i != j)
-				{
-					temp = array[i];
-					array[i] = array[j];
-					array[j] = temp;
-					print_array(array, size);
-				}
-			}
-		}
-		temp = array[i + 1];
-		array[i + 1] = array[high];
-		array[high] = temp;
-		print_array(array, size);
-		quick_sort_h(array, low, i, size);
-		quick_sort_h(array, i + 1, high, size);
-	}
-}
-
-/**
  * quick_sort - sort list by comparison
  * @array: numbers to be sorted
  * @size: volume of numbers
  */
 void quick_sort(int *array, size_t size)
 {
-	quick_sort_h(array, 0, size - 1, size);
+	int pivot;
+	int i;
+	size_t j = 0;
+	int temp;
+
+	if (size < 2)
+	{
+		return;
+	}
+
+	pivot = array[size - 1];
+	i = -1;
+
+	for (j = 0; j < size - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+	}
+
+	temp = array[i + 1];
+	array[i + 1] = pivot;
+	array[size - 1] = temp;
+	print_array(array, size);
+	quick_sort(array, i + 1);
+	quick_sort(array + i + 1, size - i - 1);
 }
