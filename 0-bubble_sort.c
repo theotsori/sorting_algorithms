f91172c914c3649d6939d98b8552ce598e0feb45
theotsori <theodoretsori@gmail.com>
2023-01-25 12:39:08 +0300

    Bubble sort update x1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,6 +11,11 @@ void bubble_sort(int *array, size_t size)
 	size_t i;
 	size_t j;
 
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
